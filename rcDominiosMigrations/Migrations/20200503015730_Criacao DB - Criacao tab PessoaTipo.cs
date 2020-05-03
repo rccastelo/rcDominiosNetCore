346220605cf4dd476d4a84a83ae0f42bae98fe7d
Renato da Costa Castelo <rccastelo@gmail.com>
2020-05-02 23:08:07 -0300

    DB - Migrations - Criacao DB e Criacao tab PessoaTipo

---
+++ rcDominiosMigrations/Migrations/20200503015730_Criacao DB - Criacao tab PessoaTipo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rcDominiosMigrations.Migrations
+{
+    public partial class CriacaoDBCriacaotabPessoaTipo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PessoaTipo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: true),
+                    Codigo = table.Column<string>(nullable: true),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Criacao = table.Column<DateTime>(nullable: false),
+                    Alteracao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PessoaTipo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PessoaTipo");
+        }
+    }
+}
