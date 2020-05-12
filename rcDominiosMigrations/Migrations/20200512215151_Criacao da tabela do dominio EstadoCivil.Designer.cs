0fabb1960dfdc09e69bbd50f54feea8e82bb1beb
Renato da Costa Castelo <rccastelo@gmail.com>
2020-05-12 18:54:40 -0300

    Criacao da tabela do dominio EstadoCivil

--- rcDominiosMigrations/Migrations/20200512215151_Criacao da tabela do dominio EstadoCivil.Designer.cs
+++ rcDominiosMigrations/Migrations/20200512215151_Criacao da tabela do dominio EstadoCivil.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using rcDominiosMigrations;
 
 namespace rcDominiosMigrations.Migrations
 {
     [DbContext(typeof(MigrationsDbContext))]
-    partial class MigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200512215151_Criacao da tabela do dominio EstadoCivil")]
+    partial class CriacaodatabeladodominioEstadoCivil
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
