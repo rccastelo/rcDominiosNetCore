b42ab95a80d72c76e672f7e46e2d24a4f2a2a544
Renato da Costa Castelo <rccastelo@gmail.com>
2020-05-12 20:06:28 -0300

    Criacao da tabela do dominio Sexo

---
+++ rcDominiosMigrations/Migrations/20200512230455_Criacao da tabela do dominio Sexo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rcDominiosMigrations.Migrations
+{
+    public partial class CriacaodatabeladodominioSexo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sexo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(maxLength: 200, nullable: true),
+                    Codigo = table.Column<string>(maxLength: 20, nullable: true),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Criacao = table.Column<DateTime>(nullable: false),
+                    Alteracao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sexo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sexo");
+        }
+    }
+}
