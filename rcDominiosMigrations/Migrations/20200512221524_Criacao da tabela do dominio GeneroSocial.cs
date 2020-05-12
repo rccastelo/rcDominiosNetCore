a731b81a96d80e5e10f7814bb25d6e3f62280823
Renato da Costa Castelo <rccastelo@gmail.com>
2020-05-12 19:27:21 -0300

    Criacao de tabela do dominio GeneroSocial

---
+++ rcDominiosMigrations/Migrations/20200512221524_Criacao da tabela do dominio GeneroSocial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rcDominiosMigrations.Migrations
+{
+    public partial class CriacaodatabeladodominioGeneroSocial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GeneroSocial",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(maxLength: 200, nullable: true),
+                    Codigo = table.Column<string>(maxLength: 20, nullable: true),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Criacao = table.Column<DateTime>(nullable: false),
+                    Alteracao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GeneroSocial", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GeneroSocial");
+        }
+    }
+}
