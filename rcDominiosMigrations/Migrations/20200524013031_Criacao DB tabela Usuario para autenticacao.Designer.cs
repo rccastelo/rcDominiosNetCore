7ba934db4797bd5d5d71f9187f87e0a1518e5295
Renato da Costa Castelo <rccastelo@gmail.com>
2020-05-24 16:14:36 -0300

    Criacao db tabela Usuario para autenticacao

--- rcDominiosMigrations/Migrations/20200524013031_Criacao DB tabela Usuario para autenticacao.Designer.cs
+++ rcDominiosMigrations/Migrations/20200524013031_Criacao DB tabela Usuario para autenticacao.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using rcDominiosMigrations;
 
 namespace rcDominiosMigrations.Migrations
 {
     [DbContext(typeof(MigrationsDbContext))]
-    partial class MigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200524013031_Criacao DB tabela Usuario para autenticacao")]
+    partial class CriacaoDBtabelaUsuarioparaautenticacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ rcDominiosMigrations/Migrations/20200524013031_Criacao DB tabela Usuario para autenticacao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rcDominiosMigrations.Migrations
+{
+    public partial class CriacaoDBtabelaUsuarioparaautenticacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Apelido = table.Column<string>(maxLength: 40, nullable: true),
+                    Senha = table.Column<string>(maxLength: 40, nullable: true),
+                    NomeApresentacao = table.Column<string>(maxLength: 40, nullable: true),
+                    NomeCompleto = table.Column<string>(maxLength: 200, nullable: true),
+                    Ativo = table.Column<bool>(nullable: false),
+                    Criacao = table.Column<DateTime>(nullable: false),
+                    Alteracao = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
